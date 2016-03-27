4be016403ce4e10864e80253f805a77731ac4aff
Ian Chen <ianchen28@gmail.com>
2016-03-28 00:21:00 +0800

    change formation

--- svm/data.cpp
+++ svm/data.cpp
@@ -38,20 +38,20 @@ using namespace std;
 // Compat
 
 int load_datafile(const char *filename, 
-                  xvec_t &xp, yvec_t &yp, int &maxd,
-                  bool norm, int maxn)
+			xvec_t &xp, yvec_t &yp, int &maxd,
+			bool norm, int maxn)
 {
-  Loader loader(filename);
-  int maxdim = 0;
-  int pcount, ncount;
-  loader.load(xp, yp, norm, maxn, &maxdim, &pcount, &ncount);
-  if (pcount + ncount > 0)
-    cout << "# Read " << pcount << "+" << ncount 
-         << "=" << pcount+ncount << " examples " 
-         << "from \"" << filename << "\"." << endl;
-  if (maxd < maxdim)
-    maxd = maxdim;
-  return pcount + ncount;
+	Loader loader(filename);
+	int maxdim = 0;
+	int pcount, ncount;
+	loader.load(xp, yp, norm, maxn, &maxdim, &pcount, &ncount);
+	if (pcount + ncount > 0)
+	  cout << "# Read " << pcount << "+" << ncount 
+		  << "=" << pcount+ncount << " examples " 
+		  << "from \"" << filename << "\"." << endl;
+	if (maxd < maxdim)
+	  maxd = maxdim;
+	return pcount + ncount;
 }
 
 
@@ -60,93 +60,93 @@ int load_datafile(const char *filename,
 
 struct Loader::Private
 {
-  string filename;
-  bool compressed;
-  bool binary;
-  igzstream gs;
-  ifstream fs;
+	string filename;
+	bool compressed;
+	bool binary;
+	igzstream gs;
+	ifstream fs;
 };
 
 Loader::~Loader()
 {
-  delete p;
+	delete p;
 }
 
 Loader::Loader(const char *name)
-  : p(new Private)
+: p(new Private)
 {
-  p->filename = name;
-  p->compressed = p->binary = false;
-  int len = p->filename.size();
-  if (len > 7 && p->filename.substr(len-7) == ".txt.gz")
-    p->compressed = true;
-  else if (len > 7 && p->filename.substr(len-7) == ".bin.gz")
-    p->compressed = p->binary = true;
-  else if (len > 4 && p->filename.substr(len-4) == ".bin")
-    p->binary = true;
-  else if (len > 4 && p->filename.substr(len-4) == ".txt")
-    p->binary = false;
-  else
-    assertfail("Filename suffix should be one of: "
-               << ".bin, .txt, .bin.gz, .txt.gz");
-  if (p->compressed)
-    p->gs.open(name);
-  else
-    p->fs.open(name);
-  if (! (p->compressed ? p->gs.good() : p->fs.good()))
-    assertfail("Cannot open " << p->filename);
+	p->filename = name;
+	p->compressed = p->binary = false;
+	int len = p->filename.size();
+	if (len > 7 && p->filename.substr(len-7) == ".txt.gz")
+	  p->compressed = true;
+	else if (len > 7 && p->filename.substr(len-7) == ".bin.gz")
+	  p->compressed = p->binary = true;
+	else if (len > 4 && p->filename.substr(len-4) == ".bin")
+	  p->binary = true;
+	else if (len > 4 && p->filename.substr(len-4) == ".txt")
+	  p->binary = false;
+	else
+	  assertfail("Filename suffix should be one of: "
+				  << ".bin, .txt, .bin.gz, .txt.gz");
+	if (p->compressed)
+	  p->gs.open(name);
+	else
+	  p->fs.open(name);
+	if (! (p->compressed ? p->gs.good() : p->fs.good()))
+	  assertfail("Cannot open " << p->filename);
 }
 
 
 int Loader::load(xvec_t &xp, yvec_t &yp, bool normalize, int maxrows,
-                 int *p_maxdim, int *p_pcount, int *p_ncount)
+			int *p_maxdim, int *p_pcount, int *p_ncount)
 {
-  istream &f = (p->compressed) ? (istream&)(p->gs) : (istream&)(p->fs);
-  bool binary = p->binary;
-  int ncount = 0;
-  int pcount = 0;
-  while (f.good() && maxrows--)
-    {
-      SVector x;
-      double y;
-      if (binary)
-        {
-          y = (f.get()) ? +1 : -1;
-          x.load(f);
-        }
-      else
-        {
-          f >> std::skipws >> y >> std::ws;
-          if (f.peek() == '|') f.get();
-          f >> x;
-        }
-      if (f.good())
-        {
-          if (normalize)
-            {
-              double d = dot(x,x);
-              if (d > 0 && d != 1.0)
-                x.scale(1.0 / sqrt(d)); 
-            }
-          if (y != +1 && y != -1)
-            assertfail("Label should be +1 or -1.");
-          xp.push_back(x);
-          yp.push_back(y);
-          if (y > 0)
-            pcount += 1;
-          else
-            ncount += 1;
-          if (p_maxdim && x.size() > *p_maxdim)
-            *p_maxdim = x.size();
-        }
-    }
-  if (!f.eof() && !f.good())
-    assertfail("Cannot read data from " << p->filename);
-  if (p_pcount)
-    *p_pcount = pcount;
-  if (p_ncount)
-    *p_ncount = ncount;
-  return pcount + ncount;
+	istream &f = (p->compressed) ? (istream&)(p->gs) : (istream&)(p->fs);
+	bool binary = p->binary;
+	int ncount = 0;
+	int pcount = 0;
+	while (f.good() && maxrows--)
+	{
+		SVector x;
+		double y;
+		if (binary)
+		{
+			y = (f.get()) ? +1 : -1;
+			x.load(f);
+		}
+		else
+		{
+			f >> std::skipws >> y >> std::ws;
+			if (f.peek() == '|') f.get();
+			f >> x;
+		}
+		if (f.good())
+		{
+			if (normalize)
+			{
+				double d = dot(x,x);
+				if (d > 0 && d != 1.0)
+				  x.scale(1.0 / sqrt(d)); 
+			}
+			if (y != +1 && y != -1)
+			  assertfail("Label should be +1 or -1.");
+			xp.push_back(x);
+			yp.push_back(y);
+			if (y > 0)
+			  pcount += 1;
+			else
+			  ncount += 1;
+			if (p_maxdim && x.size() > *p_maxdim)
+			  *p_maxdim = x.size();
+		}
+	}
+	if (!f.eof() && !f.good())
+	  assertfail("Cannot read data from " << p->filename);
+	if (p_pcount)
+	  *p_pcount = pcount;
+	if (p_ncount)
+	  *p_ncount = ncount;
+	return pcount + ncount;
 }
 
 
--- svm/svmimplicit.cpp
+++ svm/svmimplicit.cpp
@@ -70,69 +70,66 @@ using namespace std;
 // ---- Plain stochastic gradient descent with implicit updates.
 class SvmIsgd
 {
-public:
-	SvmIsgd(int dim, double lambda, double eta0  = 0);
-	void renorm();
-	double wnorm();
-	double testOne(const SVector &x, double y, double *ploss, double *pnerr);
-	void trainOne(const SVector &x, double y, double eta);
-public:
-	void train(int imin, int imax, const xvec_t &x, const yvec_t &y, const char *prefix = "");
-	void test(int imin, int imax, const xvec_t &x, const yvec_t &y, const char *prefix = "");
-public:
-	double evaluateEta(int imin, int imax, const xvec_t &x, const yvec_t &y, double eta);
-	void determineEta0(int imin, int imax, const xvec_t &x, const yvec_t &y);
-private:
-	double  lambda;
-	double  eta0;
-	FVector w;
-	double  wDivisor;
-	double  wBias;
-	double  t;
+    public:
+        SvmIsgd(int dim, double lambda, double eta0  = 0);
+        void renorm();
+        double wnorm();
+        double testOne(const SVector &x, double y, double *ploss, double *pnerr);
+        void trainOne(const SVector &x, double y, double eta);
+    public:
+        void train(int imin, int imax, const xvec_t &x, const yvec_t &y, const char *prefix = "");
+        void test(int imin, int imax, const xvec_t &x, const yvec_t &y, const char *prefix = "");
+    public:
+        double evaluateEta(int imin, int imax, const xvec_t &x, const yvec_t &y, double eta);
+        void determineEta0(int imin, int imax, const xvec_t &x, const yvec_t &y);
+    private:
+        double  lambda;
+        double  eta0;
+        FVector w;
+        double  wDivisor;
+        double  wBias;
+        double  t;
 };
 
 /// Constructor
 SvmIsgd::SvmIsgd(int dim, double lambda, double eta0)
-	: lambda(lambda), eta0(eta0),
-		w(dim), wDivisor(1), wBias(0),
-		t(0)
+: lambda(lambda), eta0(eta0),
+    w(dim), wDivisor(1), wBias(0),
+    t(0)
 {
 }
 
 /// Renormalize the weights
-void
-SvmIsgd::renorm()
+void SvmIsgd::renorm()
 {
-	cout << "\n\t Renormalizing...";
-	if (wDivisor != 1.0)
-	{
-		w.scale(1.0 / wDivisor);
-		wDivisor = 1.0;
-	}
+    cout << "\n\t Renormalizing...";
+    if (wDivisor != 1.0)
+    {
+        w.scale(1.0 / wDivisor);
+        wDivisor = 1.0;
+    }
 }
 
 /// Compute the norm of the weights
-double
-SvmIsgd::wnorm()
+double SvmIsgd::wnorm()
 {
-	double norm = dot(w,w) / wDivisor / wDivisor;
+    double norm = dot(w,w) / wDivisor / wDivisor;
 #if REGULARIZED_BIAS
-	norm += wBias * wBias
+    norm += wBias * wBias
 #endif
-		return norm;
+        return norm;
 }
 
 /// Compute the output for one example.
-double
-SvmIsgd::testOne(const SVector &x, double y, double *ploss, double *pnerr)
+double SvmIsgd::testOne(const SVector &x, double y, double *ploss, double *pnerr)
 {
-	double s = dot(w,x) / wDivisor + wBias;
-	// cout << s << " real " << y << "\n";
-	if (ploss)
-	  *ploss += LOSS::loss(s, y);
-	if (pnerr)
-	  *pnerr += (s * y <= 0) ? 1 : 0;
-	return s;
+    double s = dot(w,x) / wDivisor + wBias;
+    // cout << s << " real " << y << "\n";
+    if (ploss)
+        *ploss += LOSS::loss(s, y);
+    if (pnerr)
+        *pnerr += (s * y <= 0) ? 1 : 0;
+    return s;
 }
 
 /// Perform one iteration of the Implicit algorithm with specified gains
@@ -140,149 +137,141 @@ SvmIsgd::testOne(const SVector &x, double y, double *ploss, double *pnerr)
 /// SGD implementation.
 void SvmIsgd::trainOne(const SVector &x, double y, double eta)
 {
-	// HingeLoss case.
-	if(LOSS::name().compare("HingeLoss")==0) 
-	{
-		double ypred = dot(x, w) / wDivisor;
-		double implicitFactor = (1 + lambda * eta);
-		if(1 - y * ypred / implicitFactor < 0)
-		{
-			wDivisor *= implicitFactor;
-			// Update will be W_n+1 = Wn  / (1+lambda * eta)
-		}
-		else
-		{
-			double ypred = 0;  // computes x_t' theta_{t+1} (next update)
-			for(const SVector::Pair *p = x; p->i >= 0; p++)
-			{
-				double w_i = w.get(p->i) / wDivisor;
-				ypred += p->v * (w_i + p->v * eta * y);
-			}
+    // HingeLoss case.
+    if(LOSS::name().compare("HingeLoss")==0) 
+    {
+        double ypred = dot(x, w) / wDivisor;
+        double implicitFactor = (1 + lambda * eta);
+        if(1 - y * ypred / implicitFactor < 0)
+        {
+            wDivisor *= implicitFactor;
+            // Update will be W_n+1 = Wn  / (1+lambda * eta)
+        }
+        else
+        {
+            double ypred = 0;  // computes x_t' theta_{t+1} (next update)
+            for(const SVector::Pair *p = x; p->i >= 0; p++)
+            {
+                double w_i = w.get(p->i) / wDivisor;
+                ypred += p->v * (w_i + p->v * eta * y);
+            }
 
-			if(1 - y * ypred / implicitFactor >= 0)
-			{
-				w.add(x, eta * y * wDivisor);
-				wDivisor *= implicitFactor;
-				// Update should be theta_{t+!1} = (1/(1+lambda eta)) * (theta_t + eta * yt * xt)
-			}
-			else
-			{
-				// do nothing (no update in parameters).
-			}
-		}
+            if(1 - y * ypred / implicitFactor >= 0)
+            {
+                w.add(x, eta * y * wDivisor);
+                wDivisor *= implicitFactor;
+                // Update should be theta_{t+!1} = (1/(1+lambda eta)) * (theta_t + eta * yt * xt)
+            }
+            else
+            {
+                // do nothing (no update in parameters).
+            }
+        }
 
-		if (wDivisor > 1e5) renorm();
-	}
+        if (wDivisor > 1e5) renorm();
+    }
 
-	else if(LOSS::name().compare("LogLoss")==0) {
-		// Need to solve  ξ_t = at (yt - h(theta_t' xt + ξt ||xt||^2))
-		// Solve approximately by using
-		// ξt = (1 / (1 + at ||xt||^2 h'(theta_t'xt)) * at * (yt - h(theta_t' xt))
-		// TODO(ptoulis): Use implicit Algorithm 1 of (Toulis, et.al., ICML14)
-		double wx = dot(w, x) / wDivisor;
-		double ypred = 2 * (exp(wx) / (1 + exp(wx))) - 1;
-		double implicitFactor = 1 + eta * dot(x, x) * ypred / (1 + exp(wx));
+    else if(LOSS::name().compare("LogLoss")==0) {
+        // Need to solve  ξ_t = at (yt - h(theta_t' xt + ξt ||xt||^2))
+        // Solve approximately by using
+        // ξt = (1 / (1 + at ||xt||^2 h'(theta_t'xt)) * at * (yt - h(theta_t' xt))
+        // TODO(ptoulis): Use implicit Algorithm 1 of (Toulis, et.al., ICML14)
+        double wx = dot(w, x) / wDivisor;
+        double ypred = 2 * (exp(wx) / (1 + exp(wx))) - 1;
+        double implicitFactor = 1 + eta * dot(x, x) * ypred / (1 + exp(wx));
 
-		double ksi_t = (1 / implicitFactor) * eta * (y - ypred);
-		w.add(x,  wDivisor * ksi_t);
-	}
+        double ksi_t = (1 / implicitFactor) * eta * (y - ypred);
+        w.add(x,  wDivisor * ksi_t);
+    }
 
-	else {
-		cout << "#" << LOSS::name() << "# -- loss not found.";
-	}
+    else {
+        cout << "#" << LOSS::name() << "# -- loss not found.";
+    }
 }
 
-
 /// Perform a training epoch
-void
-SvmIsgd::train(int imin, int imax, const xvec_t &xp, const yvec_t &yp, const char *prefix)
+void SvmIsgd::train(int imin, int imax, const xvec_t &xp, const yvec_t &yp, const char *prefix)
 {
-	cout << prefix << "Training on [" << imin << ", " << imax << "]." << endl;
-	assert(imin <= imax);
-	assert(eta0 > 0);
-	for (int i=imin; i<=imax; i++)
-	{
-		double eta = eta0 / (1 + lambda * eta0 * t);
-		trainOne(xp.at(i), yp.at(i), eta);
-		t += 1;
-	}
-	cout << prefix << setprecision(6) << "wNorm=" << wnorm();
+    cout << prefix << "Training on [" << imin << ", " << imax << "]." << endl;
+    assert(imin <= imax);
+    assert(eta0 > 0);
+    for (int i=imin; i<=imax; i++)
+    {
+        double eta = eta0 / (1 + lambda * eta0 * t);
+        trainOne(xp.at(i), yp.at(i), eta);
+        t += 1;
+    }
+    cout << prefix << setprecision(6) << "wNorm=" << wnorm();
 #if BIAS
-	cout << " wBias=" << wBias;
+    cout << " wBias=" << wBias;
 #endif
-	cout << endl;
+    cout << endl;
 }
 
 /// Perform a test pass
-void
-SvmIsgd::test(int imin, int imax, const xvec_t &xp, const yvec_t &yp, const char *prefix)
+void SvmIsgd::test(int imin, int imax, const xvec_t &xp, const yvec_t &yp, const char *prefix)
 {
-	cout << prefix << "Testing on [" << imin << ", " << imax << "]." << endl;
-	assert(imin <= imax);
-	double nerr = 0;
-	double loss = 0;
-	for (int i=imin; i<=imax; i++)
-	  testOne(xp.at(i), yp.at(i), &loss, &nerr);
-	nerr = nerr / (imax - imin + 1);
-	loss = loss / (imax - imin + 1);
-	double cost = loss + 0.5 * lambda * wnorm();
-	cout << prefix
-		<< "Loss=" << setprecision(12) << loss
-		<< " Cost=" << setprecision(12) << cost
-		<< " Misclassification=" << setprecision(4) << 100 * nerr << "%."
-		<< endl;
+    cout << prefix << "Testing on [" << imin << ", " << imax << "]." << endl;
+    assert(imin <= imax);
+    double nerr = 0;
+    double loss = 0;
+    for (int i=imin; i<=imax; i++)
+        testOne(xp.at(i), yp.at(i), &loss, &nerr);
+    nerr = nerr / (imax - imin + 1);
+    loss = loss / (imax - imin + 1);
+    double cost = loss + 0.5 * lambda * wnorm();
+    cout << prefix
+        << "Loss=" << setprecision(12) << loss
+        << " Cost=" << setprecision(12) << cost
+        << " Misclassification=" << setprecision(4) << 100 * nerr << "%."
+        << endl;
 }
 
 /// Perform one epoch with fixed eta and return cost
-
-double
-SvmIsgd::evaluateEta(int imin, int imax, const xvec_t &xp, const yvec_t &yp, double eta)
+double SvmIsgd::evaluateEta(int imin, int imax, const xvec_t &xp, const yvec_t &yp, double eta)
 {
-	SvmIsgd clone(*this); // take a copy of the current state
-	assert(imin <= imax);
-	for (int i=imin; i<=imax; i++)
-	  clone.trainOne(xp.at(i), yp.at(i), eta);
-	double loss = 0;
-	double cost = 0;
-	for (int i=imin; i<=imax; i++)
-	  clone.testOne(xp.at(i), yp.at(i), &loss, 0);
-	loss = loss / (imax - imin + 1);
-	cost = loss + 0.5 * lambda * clone.wnorm();
-	// cout << "Trying eta=" << eta << " yields cost " << cost << endl;
-	return cost;
+    SvmIsgd clone(*this); // take a copy of the current state
+    assert(imin <= imax);
+    for (int i=imin; i<=imax; i++)
+        clone.trainOne(xp.at(i), yp.at(i), eta);
+    double loss = 0;
+    double cost = 0;
+    for (int i=imin; i<=imax; i++)
+        clone.testOne(xp.at(i), yp.at(i), &loss, 0);
+    loss = loss / (imax - imin + 1);
+    cost = loss + 0.5 * lambda * clone.wnorm();
+    // cout << "Trying eta=" << eta << " yields cost " << cost << endl;
+    return cost;
 }
 
-void
-SvmIsgd::determineEta0(int imin, int imax, const xvec_t &xp, const yvec_t &yp)
+void SvmIsgd::determineEta0(int imin, int imax, const xvec_t &xp, const yvec_t &yp)
 {
-	const double factor = 2.0;
-	double loEta = 1;
-	double loCost = evaluateEta(imin, imax, xp, yp, loEta);
-	double hiEta = loEta * factor;
-	double hiCost = evaluateEta(imin, imax, xp, yp, hiEta);
-	if (loCost < hiCost)
-	  while (loCost < hiCost)
-	  {
-		  hiEta = loEta;
-		  hiCost = loCost;
-		  loEta = hiEta / factor;
-		  loCost = evaluateEta(imin, imax, xp, yp, loEta);
-	  }
-	else if (hiCost < loCost)
-	  while (hiCost < loCost)
-	  {
-		  loEta = hiEta;
-		  loCost = hiCost;
-		  hiEta = loEta * factor;
-		  hiCost = evaluateEta(imin, imax, xp, yp, hiEta);
-	  }
-	eta0 = loEta;
-	cout << "# Using eta0=" << eta0 << endl;
+    const double factor = 2.0;
+    double loEta = 1;
+    double loCost = evaluateEta(imin, imax, xp, yp, loEta);
+    double hiEta = loEta * factor;
+    double hiCost = evaluateEta(imin, imax, xp, yp, hiEta);
+    if (loCost < hiCost)
+        while (loCost < hiCost)
+        {
+            hiEta = loEta;
+            hiCost = loCost;
+            loEta = hiEta / factor;
+            loCost = evaluateEta(imin, imax, xp, yp, loEta);
+        }
+    else if (hiCost < loCost)
+        while (hiCost < loCost)
+        {
+            loEta = hiEta;
+            loCost = hiCost;
+            hiEta = loEta * factor;
+            hiCost = evaluateEta(imin, imax, xp, yp, hiEta);
+        }
+    eta0 = loEta;
+    cout << "# Using eta0=" << eta0 << endl;
 }
-
 
 // --- Command line arguments
-
 const char *trainfile = 0;
 const char *testfile = 0;
 bool normalize = true;
@@ -290,100 +279,95 @@ double lambda = 1e-5;
 int epochs = 5;
 int maxtrain = -1;
 
-
-void
-usage(const char *progname)
+void usage(const char *progname)
 {
-	const char *s = ::strchr(progname,'/');
-	progname = (s) ? s + 1 : progname;
-	cerr << "Usage: " << progname << " [options] trainfile [testfile]" << endl
-		<< "Options:" << endl;
+    const char *s = ::strchr(progname,'/');
+    progname = (s) ? s + 1 : progname;
+    cerr << "Usage: " << progname << " [options] trainfile [testfile]" << endl
+        << "Options:" << endl;
 #define NAM(n) "    " << setw(16) << left << n << setw(0) << ": "
 #define DEF(v) " (default: " << v << ".)"
-	cerr << NAM("-lambda x")
-		<< "Regularization parameter" << DEF(lambda) << endl
-		<< NAM("-epochs n")
-		<< "Number of training epochs" << DEF(epochs) << endl
-		<< NAM("-dontnormalize")
-		<< "Do not normalize the L2 norm of patterns." << endl
-		<< NAM("-maxtrain n")
-		<< "Restrict training set to n examples." << endl;
+    cerr << NAM("-lambda x")
+        << "Regularization parameter" << DEF(lambda) << endl
+        << NAM("-epochs n")
+        << "Number of training epochs" << DEF(epochs) << endl
+        << NAM("-dontnormalize")
+        << "Do not normalize the L2 norm of patterns." << endl
+        << NAM("-maxtrain n")
+        << "Restrict training set to n examples." << endl;
 #undef NAM
 #undef DEF
-	::exit(10);
+    ::exit(10);
 }
 
-void
-parse(int argc, const char **argv)
+void parse(int argc, const char **argv)
 {
-	for (int i=1; i<argc; i++)
-	{
-		const char *arg = argv[i];
-		if (arg[0] != '-')
-		{
-			if (trainfile == 0)
-			  trainfile = arg;
-			else if (testfile == 0)
-			  testfile = arg;
-			else
-			  usage(argv[0]);
-		}
-		else
-		{
-			while (arg[0] == '-')
-			  arg += 1;
-			string opt = arg;
-			if (opt == "lambda" && i+1<argc)
-			{
-				lambda = atof(argv[++i]);
-				assert(lambda>0 && lambda<1e4);
-			}
-			else if (opt == "epochs" && i+1<argc)
-			{
-				epochs = atoi(argv[++i]);
-				assert(epochs>0 && epochs<1e6);
-			}
-			else if (opt == "dontnormalize")
-			{
-				normalize = false;
-			}
-			else if (opt == "maxtrain" && i+1 < argc)
-			{
-				maxtrain = atoi(argv[++i]);
-				assert(maxtrain > 0);
-			}
-			else
-			{
-				cerr << "Option " << argv[i] << " not recognized." << endl;
-				usage(argv[0]);
-			}
+    for (int i=1; i<argc; i++)
+    {
+        const char *arg = argv[i];
+        if (arg[0] != '-')
+        {
+            if (trainfile == 0)
+                trainfile = arg;
+            else if (testfile == 0)
+                testfile = arg;
+            else
+                usage(argv[0]);
+        }
+        else
+        {
+            while (arg[0] == '-')
+                arg += 1;
+            string opt = arg;
+            if (opt == "lambda" && i+1<argc)
+            {
+                lambda = atof(argv[++i]);
+                assert(lambda>0 && lambda<1e4);
+            }
+            else if (opt == "epochs" && i+1<argc)
+            {
+                epochs = atoi(argv[++i]);
+                assert(epochs>0 && epochs<1e6);
+            }
+            else if (opt == "dontnormalize")
+            {
+                normalize = false;
+            }
+            else if (opt == "maxtrain" && i+1 < argc)
+            {
+                maxtrain = atoi(argv[++i]);
+                assert(maxtrain > 0);
+            }
+            else
+            {
+                cerr << "Option " << argv[i] << " not recognized." << endl;
+                usage(argv[0]);
+            }
 
-		}
-	}
-	if (! trainfile)
-	  usage(argv[0]);
+        }
+    }
+    if (! trainfile)
+        usage(argv[0]);
 }
 
-void
-config(const char *progname)
+void config(const char *progname)
 {
-	cout << "# Running: " << progname;
-	cout << " -lambda " << lambda;
-	cout << " -epochs " << epochs;
-	if (! normalize) cout << " -dontnormalize";
-	if (maxtrain > 0) cout << " -maxtrain " << maxtrain;
-	cout << endl;
+    cout << "# Running: " << progname;
+    cout << " -lambda " << lambda;
+    cout << " -epochs " << epochs;
+    if (! normalize) cout << " -dontnormalize";
+    if (maxtrain > 0) cout << " -maxtrain " << maxtrain;
+    cout << endl;
 #define NAME(x) #x
 #define NAME2(x) NAME(x)
-	cout << "# Compiled with: "
-		<< " -DLOSS=" << NAME2(LOSS)
-		<< " -DBIAS=" << BIAS
-		<< " -DREGULARIZED_BIAS=" << REGULARIZED_BIAS
-		<< endl;
+    cout << "# Compiled with: "
+        << " -DLOSS=" << NAME2(LOSS)
+        << " -DBIAS=" << BIAS
+        << " -DREGULARIZED_BIAS=" << REGULARIZED_BIAS
+        << endl;
 }
 
 // --- main function
-
 int dims;
 xvec_t xtrain;
 yvec_t ytrain;
@@ -392,38 +376,38 @@ yvec_t ytest;
 
 int main(int argc, const char **argv)
 {
-	parse(argc, argv);
-	config(argv[0]);
-	if (trainfile)
-	  load_datafile(trainfile, xtrain, ytrain, dims, normalize, maxtrain);
-	if (testfile)
-	  load_datafile(testfile, xtest, ytest, dims, normalize);
-	cout << "# Number of features " << dims << "." << endl;
-	// prepare svm
-	int imin = 0;
-	int imax = xtrain.size() - 1;
-	int tmin = 0;
-	int tmax = xtest.size() - 1;
-	SvmIsgd svm(dims, lambda);
-	Timer timer;
-	// determine eta0 using sample
-	int smin = 0;
-	int smax = imin + min(1000, imax);
-	timer.start();
-	svm.determineEta0(smin, smax, xtrain, ytrain);
-	timer.stop();
-	// train
-	for(int i=0; i<epochs; i++)
-	{
-		cout << "--------- Epoch " << i+1 << "." << endl;
-		timer.start();
-		svm.train(imin, imax, xtrain, ytrain);
-		timer.stop();
-		cout << "Total training time " << setprecision(6)
-			<< timer.elapsed() << " secs." << endl;
-		svm.test(imin, imax, xtrain, ytrain, "train: ");
-		if (tmax >= tmin)
-		  svm.test(tmin, tmax, xtest, ytest, "test:  ");
-	}
-	return 0;
+    parse(argc, argv);
+    config(argv[0]);
+    if (trainfile)
+        load_datafile(trainfile, xtrain, ytrain, dims, normalize, maxtrain);
+    if (testfile)
+        load_datafile(testfile, xtest, ytest, dims, normalize);
+    cout << "# Number of features " << dims << "." << endl;
+    // prepare svm
+    int imin = 0;
+    int imax = xtrain.size() - 1;
+    int tmin = 0;
+    int tmax = xtest.size() - 1;
+    SvmIsgd svm(dims, lambda);
+    Timer timer;
+    // determine eta0 using sample
+    int smin = 0;
+    int smax = imin + min(1000, imax);
+    timer.start();
+    svm.determineEta0(smin, smax, xtrain, ytrain);
+    timer.stop();
+    // train
+    for(int i=0; i<epochs; i++)
+    {
+        cout << "--------- Epoch " << i+1 << "." << endl;
+        timer.start();
+        svm.train(imin, imax, xtrain, ytrain);
+        timer.stop();
+        cout << "Total training time " << setprecision(6)
+            << timer.elapsed() << " secs." << endl;
+        svm.test(imin, imax, xtrain, ytrain, "train: ");
+        if (tmax >= tmin)
+            svm.test(tmin, tmax, xtest, ytest, "test:  ");
+    }
+    return 0;
 }
